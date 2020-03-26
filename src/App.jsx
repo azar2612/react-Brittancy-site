88a32d6256e7b209d218a9bc07408d250b5cd3d2
azar <asarudeen26121997@gmail.com>
2020-03-26 23:58:10 +0530

    brittancy piere price section imported

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./header/Header";
 import Menu from "./menu/Menu";
 import Slider from "./slider/Slider";
+import Price from "./prices/Price";
 
 const App = () => (
   <div className="brittancy">
     <Header h="110px" />
     <Menu list={["about me", "services", "gallery", "contacts", "pages"]} />
     <Slider />
+    <Price />
   </div>
 );
 export default App;
