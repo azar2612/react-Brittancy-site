a250a36a8125395e5e812fcbbb56425fedff5d21
azar <asarudeen26121997@gmail.com>
2020-03-27 23:38:21 +0530

    brittancy piere footer section completed

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="footer-section">
+    <div>
+      <h6>Follow Me:</h6>
+      <i className="fab fa-instagram" />
+      <i className="fab fa-facebook-f" />
+      <i className="fab fa-twitter" />
+      <p>
+        Brittany Pierce © 2019.
+        <span>Privacy Policy.</span>
+      </p>
+    </div>
+  </div>
+);
+
+export default Footer;
