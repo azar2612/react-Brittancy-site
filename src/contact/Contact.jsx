abc6da54bed22943204a911a7947b03cde96a57b
azar <asarudeen26121997@gmail.com>
2020-03-28 23:52:37 +0530

    brittancy piere contatc section updated

---
+++ src/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Contact.css";
+import Contact1 from "./Contact1";
+
+const Contact = () => (
+  <div className="contact-section">
+    <Contact1 />
+  </div>
+);
+
+export default Contact;
